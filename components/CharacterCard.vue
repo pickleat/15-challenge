ef4c1650d1e334d06bfa83284e8f202e0372c9e6
Andy Pickle <pickleat@gmail.com>
2021-08-29 16:29:28 -0500

    initial Character Card template

---
+++ components/CharacterCard.vue
@@ -0,0 +1,35 @@
+
+<template class="character-card">
+ <div class="character-button" >
+   <img :src="character.image" class="character-image" :alt="character.name"/>
+ </div>
+</template>
+
+<script>
+  // import { mapState } from 'vuex'
+
+  export default {
+    props: {
+      character: {
+        type: Object,
+        default(){
+          return {"name": "No name", "budget": 0, "image": "~/assets/images/yoda.jpg"}
+        },
+      }
+    },
+    computed: {
+      // ...mapState({
+      //   jedi: state => state.jedi.yoda
+      // })
+    },
+    methods: {
+      useJedi(e){
+        this.$store.jedi.commit('jedi/useJedi', 'yoda dies')
+
+      }, 
+      // ...mapMutations({
+
+      // })
+    }
+  }
+</script>
