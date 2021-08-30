13c946f1896b2e31744f8a6ab556aa3847e33f9a
Andy Pickle <pickleat@gmail.com>
2021-08-30 10:09:19 -0500

    adding character row component, with a bit wild default props incase there is a mistate :)

---
+++ components/CharacterRow.vue
@@ -0,0 +1,35 @@
+
+<template >
+ <div class="character-row">
+   <div class="row-amount" :class="color" >   
+    <sup class="dollar-superscript">$</sup>{{value}}
+   </div>
+   <slot></slot>
+ </div>
+</template>
+
+<script>
+  // import { mapState } from 'vuex'
+
+  export default {
+    props: {
+      value: {
+        type: Number,
+        default(){
+          return 13
+        },
+      },
+      color: {
+        type: String, 
+        required: true,
+        default(){
+          return "pink"
+        }
+      }
+    },
+    computed: {
+    },
+    methods: {
+    }
+  }
+</script>
