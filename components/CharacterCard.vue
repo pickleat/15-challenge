5d104999e8b36d226878b566b678bc509c626414
Andy Pickle <pickleat@gmail.com>
2021-09-01 10:13:12 -0500

    fixing some defaults in case the image urls break

--- components/CharacterCard.vue
+++ components/CharacterCard.vue
@@ -1,12 +1,13 @@
 
 <template class="character-card">
- <button class="character-button" :class="{'selected' : isSelected, 'disabled' : !budgetRemaining}" :disabled="!budgetRemaining" :style="{backgroundImage:`url(${character.image})`}" @click="subtractBudget(character.budget); useChar()"  >
+ <button class="character-button" :class="{'selected' : isSelected, 'disabled' : !budgetRemaining}" :disabled="!budgetRemaining" :style="{ backgroundImage: `url(${character.image ? character.image : fallbackBackgroundURL})` }"  @click="subtractBudget(character.budget); useChar()"  >
    <div class="character-name">
    </div>
  </button>
 </template>
 
 <script>
+  import yoda from 'assets/images/yoda-2.jpeg'
   import { mapState, mapMutations } from 'vuex'
 
   export default {
@@ -14,13 +15,18 @@
       character: {
         type: Object,
         default(){
-          return {"name": "No name", "budget": 0, "image": "~/assets/images/yoda.jpg"}
+          return {
+            "name": "No name", 
+            "budget": 0, 
+            "image" : null
+          }
         },
-      }
+      },
     },
     data(){
       return{
         isSelected: false,
+        fallbackBackgroundURL: yoda
       }
     },
     computed: {
